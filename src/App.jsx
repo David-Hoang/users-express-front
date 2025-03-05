27bbc5cbb287edee390a642c90f6a63aa89c3a0e
David Hoang <88292464+David-Hoang@users.noreply.github.com>
2025-03-05 11:57:14 +0100

    add error message + loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import './App.css'
 
 function App() {
     const [users, setUsers] = useState(null);
-    const name = 'Alice';
+    const [authName, setAuthName] = useState('Alice');
+    const [displayError, setDisplayError] = useState(false);
 
     const fetching = async () => {
         try {
             const fetch = await axios.get('http://localhost:8000/api/users');
             setUsers(fetch.data);
         } catch (error) {
-            console.log(error);
+            setDisplayError(true);
         }
     };
 
@@ -21,18 +22,26 @@ function App() {
 
     return (
       <>
-          <h1>Hello world and {name}</h1>
-          <div className="user-list">
-            {users && users.map(user => {
-              return (
-                <div className="single-user">
-                  <p>Nom : {user.lastName}</p>
-                  <p>Prénom : {user.firstName}</p>
-                  <p>Addresse : {user.address}</p>
-                </div>
+          <h1>Hello world and {authName}</h1>
+          <ul className="user-list">
+            {!users ? (
+              <li>Chargement...</li> )
+            : displayError ? (
+                <li>Une erreur est survenue</li> )
+              : (
+              users.map((user,index) => {
+                return (
+                  <li key={index} className="single-user">
+                    <p>Nom : {user.lastName}</p>
+                    <p>Prénom : {user.firstName}</p>
+                    <p>Addresse : {user.address}</p>
+                    <p>Téléphone : {user.telephone}</p>
+                    <p>Hobbies : {user.hobbies.map((hobbie, idx) => { return (<span key={idx}>{hobbie} </span>) } )}</p>
+                  </li>
+                )}
               )
-            })}
-          </div>
+              )}
+          </ul>
       </>
     ) 
 }
